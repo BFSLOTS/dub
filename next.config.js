006b908de23b2c446af30d044c53c88690da2b50
poonben <93625450+poonben@users.noreply.github.com>
2022-10-16 00:49:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "www.google.com",
+      "avatar.tobi.sh",
+      "faisalman.github.io",
+      "avatars.dicebear.com",
+      "res.cloudinary.com",
+    ],
+  },
+};
